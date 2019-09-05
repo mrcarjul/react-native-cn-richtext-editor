b26ff07ef1a53efd99d4e80dcf78ec6155bf79ee
Carlos <mrcarjul@gmail.com>
2019-09-05 10:58:45 -0500

    eslint identation

--- src/CNRichTextView.js
+++ src/CNRichTextView.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import {
-  View, Text, Image, TouchableWithoutFeedback,
-} from 'react-native';
+import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import _ from 'lodash';
 import { convertToObject } from './Convertors';
 import CNStyledText from './CNStyledText';
@@ -11,7 +9,7 @@ class CNRichTextView extends Component {
     super(props);
     this.state = {
       contents: [],
-      layoutWidth: 400,
+      layoutWidth: 400
     };
     this.touchX = -1;
     this.touchY = -1;
@@ -19,25 +17,25 @@ class CNRichTextView extends Component {
   }
 
   componentDidMount() {
-    const { text, styleList} = this.props;
+    const { text, styleList } = this.props;
     const styles = styleList ? styleList : null;
 
     const items = convertToObject(text, styles);
 
     this.setState({
-      contents: items,
+      contents: items
     });
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const { text, styleList} = this.props;
+    const { text, styleList } = this.props;
 
     if (prevProps.text != text) {
       const styles = styleList ? styleList : null;
       const items = convertToObject(text, styles);
 
       this.setState({
-        contents: items,
+        contents: items
       });
     }
   }
@@ -56,15 +54,12 @@ class CNRichTextView extends Component {
         key={input.id}
         style={{
           borderWidth: 0,
-          color,
+          color
         }}
       >
-        {
-                    _.map(input.content, item => (
-                      <CNStyledText key={item.id} style={item.styleList} text={item.text} />
-                    ))
-
-                }
+        {_.map(input.content, item => (
+          <CNStyledText key={item.id} style={item.styleList} text={item.text} />
+        ))}
       </Text>
     );
   }
@@ -73,15 +68,16 @@ class CNRichTextView extends Component {
     const { width, height } = image.size;
     const { layoutWidth } = this.state;
     const { ImageComponent = Image } = this.props;
-    const myHeight = (layoutWidth - 4 < width) ? height * ((layoutWidth - 4) / width) : height;
-    const myWidth = (layoutWidth - 4 < width) ? layoutWidth - 4 : width;
+    const myHeight =
+      layoutWidth - 4 < width ? height * ((layoutWidth - 4) / width) : height;
+    const myWidth = layoutWidth - 4 < width ? layoutWidth - 4 : width;
 
     return (
       <View
         key={`image${index}`}
         style={{
           flexDirection: 'row',
-          alignItems: 'flex-start',
+          alignItems: 'flex-start'
         }}
       >
         <View>
@@ -89,72 +85,60 @@ class CNRichTextView extends Component {
             style={{
               width: myWidth,
               height: myHeight,
-              opacity: this.state.imageHighLightedInex === index ? 0.8 : 1,
+              opacity: this.state.imageHighLightedInex === index ? 0.8 : 1
             }}
             source={{ uri: image.url }}
           />
         </View>
       </View>
-
     );
   }
 
-    onLayout = (event) => {
-      const {
-        x,
-        y,
-        width,
-        height,
-      } = event.nativeEvent.layout;
+  onLayout = event => {
+    const { x, y, width, height } = event.nativeEvent.layout;
 
-      this.setState({
-        layoutWidth: width - 2,
-      });
-    }
+    this.setState({
+      layoutWidth: width - 2
+    });
+  };
 
-    render() {
-      const { contents } = this.state;
-      const { style } = this.props;
+  render() {
+    const { contents } = this.state;
+    const { style } = this.props;
 
-      const styles = style || {};
-      return (
-        <View
-          onLayout={this.onLayout}
-          style={[styles]}
-          onStartShouldSetResponder={(evt) => {
-            this.touchX = evt.nativeEvent.pageX;
-            this.touchY = evt.nativeEvent.pageY;
-            return true;
-          }}
-          onResponderRelease={(evt) => {
-            if(Math.abs(evt.nativeEvent.pageX - this.touchX) < 25
-            && Math.abs(evt.nativeEvent.pageY - this.touchY) < 25
-            ) {
-               setTimeout(this.flip, 50);
-            }
+    const styles = style || {};
+    return (
+      <View
+        onLayout={this.onLayout}
+        style={[styles]}
+        onStartShouldSetResponder={evt => {
+          this.touchX = evt.nativeEvent.pageX;
+          this.touchY = evt.nativeEvent.pageY;
+          return true;
+        }}
+        onResponderRelease={evt => {
+          if (
+            Math.abs(evt.nativeEvent.pageX - this.touchX) < 25 &&
+            Math.abs(evt.nativeEvent.pageY - this.touchY) < 25
+          ) {
+            setTimeout(this.flip, 50);
+          }
 
-            this.touchX = -1;
-            this.touchY = -1;
-            
-          }}
-        >
-          {
-            _.map(contents, (item, index) => {
-              if (item.component === 'text') {
-                return (
-                  this.renderText(item, index)
-                );
-              }
-              if (item.component === 'image') {
-                return (
-                  this.renderImage(item, index)
-                );
-              }
-            })
-            }
-        </View>
-      );
-    }
+          this.touchX = -1;
+          this.touchY = -1;
+        }}
+      >
+        {_.map(contents, (item, index) => {
+          if (item.component === 'text') {
+            return this.renderText(item, index);
+          }
+          if (item.component === 'image') {
+            return this.renderImage(item, index);
+          }
+        })}
+      </View>
+    );
+  }
 }
 
 export default CNRichTextView;
